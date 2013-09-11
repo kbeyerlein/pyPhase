666546a7d8ca420587956c4272cda00bf3e50fa6
Kenneth Beyerlein <kenneth.beyerlein@desy.de>
2013-09-11 14:50:34 +0200

    Make Modulus projection accept a mask for the good pixels

--- IterPhase/src/iterPhase.py
+++ IterPhase/src/iterPhase.py
@@ -21,15 +21,6 @@ def makePositive(rho):
     posRho=np.array([[j if j>0 else 0 for j in i] for i in rho])
     return posRho
 
-def modulusProjection(rho, mod):
-    f= np.fft.fft2(np.array(rho))
-    f=mod.real*np.exp(1j*np.angle(f))
-    rho= np.fft.ifft2(f)
-    return rho
-
-def supportProjection(rho, support):
-    return np.array(rho)*np.array(support)
-
 def makeReal(arr):
     return np.array([[np.absolute(i) if i.real >=-0.000001 else -np.absolute(i) for i in row] for row in arr])
 
@@ -42,16 +33,31 @@ def makePositiveMask(arr):
 def invertMask(mask):
     return np.ones(np.shape(mask))-np.array(mask)
 
+def modulusProjection(rho, mod, mask):
+    f= np.fft.fft2(np.array(rho))
+    invmask=invertMask(mask)
+    f=(mod.real*np.exp(1j*np.angle(f)))*mask+f*invmask
+    """print "f"
+    plt.imshow(det.centerFFTImage(np.absolute(f)), interpolation='nearest')
+    plt.colorbar()
+    plt.show()
+    """
+    rho= np.fft.ifft2(f)
+    return rho
+
+def supportProjection(rho, support):
+    return np.array(rho)*np.array(support)
+
 def autoCorrelation(arr):
     return np.absolute(np.fft.ifft2(np.array(arr)))
 
-def HIO(rho, mod, support, b=0.9, positive=False, debug=False):
+def HIO(rho, mod, support, mask, b=0.9, positive=False, debug=False):
     if (debug):
         print "support"
         plt.imshow(det.centerFFTImage(support), interpolation='nearest')
         plt.colorbar()
         plt.show()
-    modRho=modulusProjection(rho, mod)
+    modRho=modulusProjection(rho, mod, mask)
     if (positive):
         support = makePositiveMask(modRho*np.array(support))
     suppModRho=modRho*np.array(support)
@@ -80,8 +86,8 @@ def HIO(rho, mod, support, b=0.9, positive=False, debug=False):
         
     return  suppModRho+hiodiff
 
-def ErrReduct(rho, modulus, support, positive=False):
-    myRho=modulusProjection(rho,modulus)
+def ErrReduct(rho, modulus, support, mask, positive=False):
+    myRho=modulusProjection(rho,modulus, mask)
     if (positive):
         support=makePositiveMask(myRho*np.array(support))
     return supportProjection(myRho, support)
